f5e0031328e447ddfb629cc8338c7d68e9479d94
unknown <nikoladimitrijevic1991@gmail.com>
2019-07-23 20:10:08 +0200

    new template

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ function instagramSectionDoubleHover() {
   if(instagramSection[0].getClientRects()[0].top < window.innerHeight / 2){
     instagramSection[0].style.background = 'rgba(0,0,0,1)';
     console.log('asd');
+  }else{
+    instagramSection[0].style.background = 'rgba(12,4,0,0.95)';
   }
 }
 
